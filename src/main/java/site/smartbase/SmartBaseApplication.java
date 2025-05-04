9eabe426aab628d29dc5e3d1b1604ba5fa47e304
Oleksandra Bulhakova <alexbulgakovapro@gmail.com>
2025-05-04 18:12:25 +0300

    First commit

---
+++ src/main/java/site/smartbase/SmartBaseApplication.java
@@ -0,0 +1,13 @@
+package site.smartbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBaseApplication.class, args);
+    }
+
+}
